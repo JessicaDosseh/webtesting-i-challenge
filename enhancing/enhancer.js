f7c6ecc8716bc8bf4b25396a5dcf78401cd5f6af
Jessica <Jessica@users-iMac.local>
2020-04-30 18:12:33 -0400

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,73 @@ module.exports = {
   get,
 };
 
+// *** Set item structure perameters *** //  
+// Items have name, durability and enhancement.
+// The item's enhancement it's a number from 0 to 20.
+// The item's durability it's a number from 0 to 100.
+function itemStructure(item) {
+  if (item.name && item.durability && item.enhancement) {
+    if (
+        item.durability >= 0 &&
+        item.durability <= 100 &&
+        item.enhancement >= 0 &&
+        item.enhancement <= 20
+    ) {
+        return true;
+    } else {
+        throw 'durability or enhancement are not within correct range.';
+    }
+  } else {
+      throw 'item must be an object with name, durability, and enhancement';
+  }
+}
+
+// success(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement success.
 function succeed(item) {
-  return { ...item };
+  if (itemStructure(item)) {
+    const newItem = { ...item };
+
+    // The item's enhancement it's a number from 0 to 20.
+    // If the item enhancement level is 20, the enhancement level is not changed.
+    // The durability of the item is not changed.
+    if (newItem.enhancement < 20) 
+        newItem.enhancement++; // The item's enhancement increases by 1.
+    return newItem;
+  }
+  return;
 }
 
+// fail(item) method that accepts an item object and returns a new item object modified according to the rules defined by the client for enhancement failure.
 function fail(item) {
-  return { ...item };
+  if (itemStructure(item)) {
+    const newItem = { ...item };
+
+    // If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+    if (newItem.enhancement < 15) {
+        newItem.durability -= 5;
+    // If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+    } else if (newItem.enhancement >= 15) {
+        newItem.durability -= 10;
+
+        // If the item's enhancement level is greater than 16, the enhancement level decreases by 1
+        if (newItem.enhancement > 16) {
+            newItem.enhancement -= 1;
+        }
+    }
+    return newItem;
+  }
+  return;
 }
 
+// repair(item) method that accepts an item object and returns a new item with the durability restored to 100.
 function repair(item) {
-  return { ...item };
+  if (itemStructure(item)) {
+    return { ...item, durability: 100 };
+  }
+  return;
 }
 
+// get() method for use when working on the stretch problem.
 function get(item) {
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,382 @@
 const enhancer = require('./enhancer.js');
 // test away!
+// test/it(name, fn, timeout); 
+
+describe('enhanceer', () => {
+
+  describe('test validation', () => {
+    test('tests that true is true', () => {
+      expect(true).toBe(true);
+    })
+  });
+
+// ----- DESCRIBE REPAIR ---------------------------------------------------
+
+  describe('repair', () => {
+
+    it('restores item durability to 100', () => {
+      // given item 
+      const item = {
+          name: 'fork',
+          durability: 40,
+          enhancement: 12,
+      };
+
+      // after calling .repair(item)
+      const expectedOutcome = {
+          name: 'fork',
+          durability: 100,
+          enhancement: 12,
+      };
+
+      const actualOutcome = enhancer.repair(item);
+
+      expect(actualOutcome.durability).toEqual(
+          expectedOutcome.durability
+      );
+    });
+
+    // ******************************************** //
+
+    it('returns a new item object', () => {
+      const item = {
+          name: 'fork',
+          durability: 40,
+          enhancement: 12,
+      };
+
+      const expectedOutcome = {
+          name: 'fork',
+          durability: 100,
+          enhancement: 12,
+      };
+
+      const actualOutcome = enhancer.repair(item);
+
+      expect(actualOutcome).toEqual(expectedOutcome);
+    });
+
+    // ******************************************** //
+
+    it('item durability is a positive number', () => {
+      const item = {
+          name: 'fork',
+          durability: 40,
+          enhancement: 12,
+      };
+
+      const actualOutcome = enhancer.repair(item);
+
+      expect(actualOutcome.durability).toBeGreaterThanOrEqual(0);
+    });
+
+    // ******************************************** //
+
+    it('after repair, item durability is not greater than 100', () => {
+      const item = {
+          name: 'fork',
+          durability: 40,
+          enhancement: 12,
+      };
+
+      const actualOutcome = enhancer.repair(item);
+
+      expect(actualOutcome.durability).toBeLessThanOrEqual(100);
+    });
+
+    // ******************************************** //
+
+    it('item enhancement is not changed', () => {
+      const item = {
+          name: 'fork',
+          durability: 60,
+          enhancement: 12,
+      };
+
+      const expectedOutcome = {
+          name: 'fork',
+          durability: 100,
+          enhancement: 12,
+      };
+
+      const actualOutcome = enhancer.repair(item);
+
+      expect(actualOutcome.enhancement).toEqual(
+          expectedOutcome.enhancement
+      );
+    });
+
+    // ******************************************** //
+  });
+
+// ----- DESCRIBE SUCCEED ---------------------------------------------------
+
+  describe('succeed', () => {
+
+    it('returns a new item object', () => {
+      const item = {
+          name: 'pen',
+          durability: 40,
+          enhancement: 10,
+      };
+
+      const expectedOutcome = {
+          name: 'pen',
+          durability: 40,
+          enhancement: 11,
+      };
+
+      const actualOutcome = enhancer.succeed(item);
+
+      expect(actualOutcome).toEqual(expectedOutcome);
+    });
+
+    // ******************************************** //
+
+    it('item enhancement is a positive number', () => {
+      const item = {
+          name: 'pen',
+          durability: 40,
+          enhancement: 10,
+      };
+
+      const actualOutcome = enhancer.succeed(item);
+
+      expect(actualOutcome.enhancement).toBeGreaterThanOrEqual(0);
+    });
+
+    // ******************************************** //
+
+    it('item enhancement is not greater than 20', () => {
+        const item = {
+            name: 'pen',
+            durability: 40,
+            enhancement: 10,
+        };
+
+        const actualOutcome = enhancer.succeed(item);
+
+        expect(actualOutcome.enhancement).toBeLessThanOrEqual(20);
+    });
+
+    // ******************************************** //
+
+    it('item enhancement increases by 1', () => {
+        const item = {
+            name: 'pen',
+            durability: 40,
+            enhancement: 10,
+        };
+
+        const expectedOutcome = {
+            name: 'pen',
+            durability: 40,
+            enhancement: 11,
+        };
+
+        const actualOutcome = enhancer.succeed(item);
+
+        expect(actualOutcome.enhancement).toBe(expectedOutcome.enhancement);
+    });
+
+    // ******************************************** //
+
+    it('item enhancement remains 20 if it was initially 20', () => {
+        const item = {
+            name: 'pen',
+            durability: 40,
+            enhancement: 20,
+        };
+
+        const expectedOutcome = {
+            name: 'pen',
+            durability: 40,
+            enhancement: 20,
+        };
+
+        const actualOutcome = enhancer.succeed(item);
+
+        expect(actualOutcome.enhancement).toBe(expectedOutcome.enhancement);
+    });
+
+    // ******************************************** //
+
+    it('item durability is a positive number', () => {
+        const item = {
+            name: 'pen',
+            durability: 40,
+            enhancement: 12,
+        };
+
+        const actualOutcome = enhancer.succeed(item);
+
+        expect(actualOutcome.durability).toBeGreaterThanOrEqual(0);
+    });
+
+    // ******************************************** //
+
+    it('item durability is not changed', () => {
+        const item = {
+            name: 'pen',
+            durability: 40,
+            enhancement: 12,
+        };
+
+        const expectedOutcome = {
+            name: 'pen',
+            durability: 40,
+            enhancement: 13,
+        };
+
+        const actualOutcome = enhancer.succeed(item);
+
+        expect(actualOutcome.durability).toEqual(
+            expectedOutcome.durability
+        );
+    });
+
+    // ******************************************** //
+  });
+
+// ----- DESCRIBE FAIL ---------------------------------------------------
+
+  describe('fail', () => {
+
+    it('returns a new item object', () => {
+      const item = {
+          name: 'cup',
+          durability: 60,
+          enhancement: 12,
+      };
+
+      const expectedOutcome = {
+          name: 'cup',
+          durability: 55,
+          enhancement: 12,
+      };
+
+      const actualOutcome = enhancer.fail(item);
+
+      expect(actualOutcome).toEqual(expectedOutcome);
+    });
+
+    // ******************************************** //
+
+
+    it('decrease item durability by 5 if enhancement was less than 15', () => {
+      const item = {
+          name: 'cup',
+          durability: 60,
+          enhancement: 12,
+      };
+
+      const actualOutcome = enhancer.fail(item);
+
+      expect(actualOutcome.durability).toBe(55);
+  });
+
+  // ******************************************** //
+
+  it('decrease item durability by 10 if enhancement was 15 or higher', () => {
+      const item = {
+          name: 'cup',
+          durability: 60,
+          enhancement: 15,
+      };
+
+      const actualOutcome = enhancer.fail(item);
+
+      expect(actualOutcome.durability).toBe(50);
+  });
+
+  // ******************************************** //
+
+  it('decrease item enhancement by 1 if it was greater than 16', () => {
+      const item = {
+          name: 'cup',
+          durability: 60,
+          enhancement: 17,
+      };
+
+      const actualOutcome = enhancer.fail(item);
+
+      expect(actualOutcome.enhancement).toBe(16);
+  });
+
+  // ******************************************** //
+
+    it('item enhancement is a positive number', () => {
+        const item = {
+            name: 'cup',
+            durability: 60,
+            enhancement: 12,
+        };
+
+        const actualOutcome = enhancer.fail(item);
+
+        expect(actualOutcome.enhancement).toBeGreaterThanOrEqual(0);
+    });
+
+    // ******************************************** //
+
+    it('item enhancement is not greater than 20', () => {
+        const item = {
+            name: 'cup',
+            durability: 60,
+            enhancement: 12,
+        };
+
+        const actualOutcome = enhancer.fail(item);
+
+        expect(actualOutcome.enhancement).toBeLessThanOrEqual(20);
+    });
+
+    // ******************************************** //
+
+    it('item durability is a positive number', () => {
+        const item = {
+            name: 'cup',
+            durability: 60,
+            enhancement: 12,
+        };
+
+        const actualOutcome = enhancer.fail(item);
+
+        expect(actualOutcome.durability).toBeGreaterThanOrEqual(0);
+    });
+
+    // ******************************************** //
+
+    it('item durability is not greater than 100', () => {
+        const item = {
+            name: 'cup',
+            durability: 60,
+            enhancement: 12,
+        };
+
+        const actualOutcome = enhancer.fail(item);
+
+        expect(actualOutcome.durability).toBeLessThanOrEqual(100);
+    });
+
+    // ******************************************** //
+  });
+
+// ----- DESCRIBE GET ITEM ---------------------------------------------------
+
+  describe('get item', () => {
+
+    it('returns the item provided', () => {
+      const item = {
+          name: 'cup',
+          durability: 40,
+          enhancement: 10,
+      };
+
+      const actualOutcome = enhancer.get(item);
+
+      expect(actualOutcome).toEqual(item);
+    });
+    
+  });
+});
